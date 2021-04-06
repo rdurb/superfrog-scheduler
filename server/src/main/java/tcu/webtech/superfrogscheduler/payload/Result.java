c8c26e32590cf3534fb7ccedcb1f339758b0a5f9
rdurb <rileydurbin2@gmail.com>
2021-04-06 16:28:06 -0500

    backend for email/password authentication completed

---
+++ server/src/main/java/tcu/webtech/superfrogscheduler/payload/Result.java
@@ -0,0 +1,26 @@
+package tcu.webtech.superfrogscheduler.payload;
+
+import lombok.Getter;
+
+@Getter
+public class Result {
+    private boolean flag;
+    private Integer code;
+    private String message;
+    private Object data;
+
+    public Result() { }
+
+    public Result(boolean flag, Integer code, String message, Object data) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Result(boolean flag, Integer code, String message) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+    }
+}

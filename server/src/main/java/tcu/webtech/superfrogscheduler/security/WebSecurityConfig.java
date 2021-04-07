903186d2005c0632cc60cb795eaf11aa3010bedb
rdurb <rileydurbin2@gmail.com>
2021-04-07 17:07:23 -0500

    added html files into server

--- server/src/main/java/tcu/webtech/superfrogscheduler/security/WebSecurityConfig.java
+++ server/src/main/java/tcu/webtech/superfrogscheduler/security/WebSecurityConfig.java
@@ -58,8 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
-                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/", "/h2-console/**").permitAll()
                 .anyRequest().authenticated();
 
         http.headers().frameOptions().sameOrigin();

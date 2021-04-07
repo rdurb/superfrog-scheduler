960bbca1050f37b5307b22f9f0901ee602535dfd
rdurb <rileydurbin2@gmail.com>
2021-04-07 17:13:48 -0500

    moved html files

--- server/src/main/java/tcu/webtech/superfrogscheduler/security/WebSecurityConfig.java
+++ server/src/main/java/tcu/webtech/superfrogscheduler/security/WebSecurityConfig.java
@@ -57,9 +57,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/", "/h2-console/**").permitAll()
-                .anyRequest().authenticated();
+                .authorizeRequests()
+                    .antMatchers("/api/auth/**").permitAll()
+                    .antMatchers("/", "/h2-console/**", "/assets/**").permitAll()
+                    .anyRequest().authenticated();
 
         http.headers().frameOptions().sameOrigin();
 
